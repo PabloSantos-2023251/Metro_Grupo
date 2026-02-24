c86193ec53158b5368c4369507c296147aa848df
DomingoRomero2025157 <dromero-2025157@kinal.edu.gt>
2026-02-24 10:18:47 -0600

    feat:se agrego clase Entity de la entidad conductores

---
+++ Backend/src/main/java/com/Metro/org/Entity/Conductores.java
@@ -0,0 +1,55 @@
+package com.Metro.org.Entity;
+
+import jakarta.persistence.*;
+
+@Entity // una clase anotada , represeta una tabla relacionar , actua comom un puente entre la programacion orientada a objetos y SQL (JPA ivernes)prermitiendo mapiar automaticamente atrivutos a columnas
+@Table(name = "Conductores")
+
+public class Conductores {
+    @Id
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_Conductores")
+    private Integer idConductores;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "licencia")
+    private Integer licencia;
+
+    @Column(name = "anos_experencia")
+    private String anosExperencia;
+
+    public Integer getIdConductores() {
+        return idConductores;
+    }
+
+    public void setIdConductores(Integer idConductores) {
+        this.idConductores = idConductores;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getAnosExperencia() {
+        return anosExperencia;
+    }
+
+    public void setAnosExperencia(String anosExperencia) {
+        this.anosExperencia = anosExperencia;
+    }
+
+    public Integer getLicencia() {
+        return licencia;
+    }
+
+    public void setLicencia(Integer licencia) {
+        this.licencia = licencia;
+    }
+}
