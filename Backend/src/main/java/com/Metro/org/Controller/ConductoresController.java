d9d0f894a0acc2e93e8068ed4a054e691f5938e0
DomingoRomero2025157 <dromero-2025157@kinal.edu.gt>
2026-02-24 10:27:25 -0600

    feat:se agrego clase Controller de la entidad conductores

---
+++ Backend/src/main/java/com/Metro/org/Controller/ConductoresController.java
@@ -0,0 +1,58 @@
+package com.Metro.org.Controller;
+
+import com.Metro.org.Entity.Conductores;
+import com.Metro.org.Service.ConductoresService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/Conductores")
+
+public class ConductoresController {
+    private final ConductoresService conductoresService;
+    public ConductoresController (ConductoresService ConductoresService){
+        this.conductoresService = ConductoresService;
+    }
+
+    @GetMapping
+    public List<Conductores> getAllConductores(){return conductoresService.getAllConductores();}
+
+    @PostMapping
+    public ResponseEntity<Object> createConductores(@Valid @RequestBody Conductores conductores){
+        try{
+            Conductores createdConductores = conductoresService.saveConductores(conductores);
+            return new ResponseEntity<>(createdConductores, HttpStatus.CREATED);
+        }catch (IllegalArgumentException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteConductores(@PathVariable Integer id){
+        try {
+            conductoresService.deleteConductores(id);
+            return ResponseEntity.ok("Conductores Eliminado Correctamente");
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateConductores(@PathVariable Integer id, @RequestBody Conductores conductores) {
+
+        try {
+            Conductores actualizado = conductoresService.updateConductores(id, conductores);
+            return ResponseEntity.ok(actualizado);
+
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+
+    }
+}
