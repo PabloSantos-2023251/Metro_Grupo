23d0b79eb46c6e82bd406761f323db272dd05562
DomingoRomero2025157 <dromero-2025157@kinal.edu.gt>
2026-02-24 10:06:12 -0600

    feat:se añadio la entidad trenes en backend

---
+++ Backend/src/main/java/com/Metro/org/Entity/Tren.java
@@ -0,0 +1,57 @@
+package com.Metro.org.Entity;
+
+
+import jakarta.persistence.*;
+
+@Entity // una clase anotada , represeta una tabla relacionar , actua comom un puente entre la programacion orientada a objetos y SQL (JPA ivernes)prermitiendo mapiar automaticamente atrivutos a columnas
+@Table(name = "Tren")
+
+public class Tren {
+    @Id
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_tren")
+    private Integer idTren;
+
+    @Column(name = "modelo")
+    private String modelo;
+
+    @Column(name = "capacidad_pasajeros")
+    private Integer capacidadPasajeros;
+
+    @Column(name = "estado")
+    private String estado;
+
+    public Integer getIdTren() {
+        return idTren;
+    }
+
+    public void setIdTren(Integer idTren) {
+        this.idTren = idTren;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public Integer getCapacidadPasajeros() {
+        return capacidadPasajeros;
+    }
+
+    public void setCapacidadPasajeros(Integer capacidadPasajeros) {
+        this.capacidadPasajeros = capacidadPasajeros;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
+
