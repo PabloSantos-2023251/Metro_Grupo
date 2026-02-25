7bd6684cba563c1d567a34cb8a81149bf4ec89a3
omartobar-2025092 <otobar-2025092@kinal.edu.gt>
2026-02-25 09:16:24 -0600

    feat: Se agrego el mapeo a la base de datos de la entidad boleto

--- Backend/src/main/java/com/Metro/org/entity/boleto.java
+++ Backend/src/main/java/com/Metro/org/entity/boleto.java
@@ -1,4 +1,59 @@
 package com.Metro.org.entity;
 
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "Boletos")
 public class boleto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_boleto")
+    private Integer idBoleto;
+
+    @Column(name = "precio", nullable = false, precision = 10, scale = 2)
+    private BigDecimal precio;
+
+    @Column(name = "fecha", nullable = false)
+    private LocalDate fecha;
+
+    @ManyToOne
+    @JoinColumn(name = "id_pasajero", nullable = false)
+    private Pasajero pasajero;
+
+    // Getters y Setters //
+
+    public Integer getIdBoleto() {
+        return idBoleto;
+    }
+
+    public void setIdBoleto(Integer idBoleto) {
+        this.idBoleto = idBoleto;
+    }
+
+    public BigDecimal getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(BigDecimal precio) {
+        this.precio = precio;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public Pasajero getPasajero() {
+        return pasajero;
+    }
+
+    public void setPasajero(Pasajero pasajero) {
+        this.pasajero = pasajero;
+    }
 }
