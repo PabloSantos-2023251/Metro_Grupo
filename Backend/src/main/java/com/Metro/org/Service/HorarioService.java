fb0d321c62d0d7ef13e93c5386b9f6ba8c5394cd
MiguelSantizo <msantizo-2022021@kinal.edu.gt>
2026-02-24 19:49:57 -0600

    feat: agregar interfaz HorarioService

---
+++ Backend/src/main/java/com/Metro/org/Service/HorarioService.java
@@ -0,0 +1,12 @@
+package com.Metro.org.Service;
+
+import com.Metro.org.Entity.Horario;
+import java.util.List;
+
+public interface HorarioService {
+    List<Horario> getAllHorarios();
+    Horario getHorarioById(Integer id);
+    Horario saveHorario(Horario horario);
+    Horario updateHorario(Integer id, Horario horario);
+    void deleteHorario(Integer id);
+}
