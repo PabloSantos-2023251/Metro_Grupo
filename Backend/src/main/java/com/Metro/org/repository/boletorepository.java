189289a158c0e19a990fed4428287ab12085d5c7
omartobar-2025092 <otobar-2025092@kinal.edu.gt>
2026-02-25 10:22:36 -0600

    feat: Se agrego el codigo a BoletosRepository

--- Backend/src/main/java/com/Metro/org/repository/boletorepository.java
+++ Backend/src/main/java/com/Metro/org/repository/boletorepository.java
@@ -1,4 +1,11 @@
 package com.Metro.org.repository;
 
-public interface boletorepository {
+import com.Metro.org.entity.boleto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface boletorepository extends JpaRepository<boleto, Integer> {
+
 }
+
