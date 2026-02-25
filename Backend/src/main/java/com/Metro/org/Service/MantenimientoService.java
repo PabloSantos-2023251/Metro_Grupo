2855a95d6091b95282eaa017d8ba5ac1a289281c
MiguelSantizo <msantizo-2022021@kinal.edu.gt>
2026-02-24 19:50:30 -0600

    feat: agregar interfaz MantenimientoService

---
+++ Backend/src/main/java/com/Metro/org/Service/MantenimientoService.java
@@ -0,0 +1,17 @@
+package com.Metro.org.Service;
+
+import com.Metro.org.Entity.Mantenimiento;
+import java.util.List;
+
+public interface MantenimientoService {
+
+    List<Mantenimiento> getAllMantenimientos();
+
+    Mantenimiento getMantenimientoById(Integer id);
+
+    Mantenimiento saveMantenimiento(Mantenimiento mantenimiento);
+
+    Mantenimiento updateMantenimiento(Integer id, Mantenimiento mantenimiento);
+
+    void deleteMantenimiento(Integer id);
+}
