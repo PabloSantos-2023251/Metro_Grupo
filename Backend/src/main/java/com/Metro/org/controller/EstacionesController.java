9508e6e1433380fdbc87cbe1bc67760ba75b66db
David Enrique Quintanilla Pérez <dquintanilla-2022275@kinal.edu.gt>
2026-02-27 16:57:51 -0600

    Feat: Se agrego la logica de las excepsiones de Estaciones

--- Backend/src/main/java/com/Metro/org/controller/EstacionesController.java
+++ Backend/src/main/java/com/Metro/org/controller/EstacionesController.java
@@ -7,6 +7,9 @@ import org.hibernate.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import java.util.HashMap;
+import java.util.Map;
 
 import java.util.List;
 
@@ -46,9 +49,12 @@ public class EstacionesController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Estaciones> updateEstaciones(@PathVariable Integer id, @RequestBody Estaciones estaciones){
-        Estaciones EstacionesActualizar = estacionesService.updateEstaciones(id, estaciones);
-        return ResponseEntity.ok(EstacionesActualizar);
+    public ResponseEntity<Estaciones> updateEstaciones(
+            @PathVariable Integer id,
+            @Valid @RequestBody Estaciones estaciones) {
+
+        Estaciones estacionesActualizar = estacionesService.updateEstaciones(id, estaciones);
+        return ResponseEntity.ok(estacionesActualizar);
     }
 
     @GetMapping("/{id}")
@@ -60,4 +66,17 @@ public class EstacionesController {
             return ResponseEntity.notFound().build();
         }
     }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> manejarErroresValidacion(
+            MethodArgumentNotValidException ex) {
+
+        Map<String, String> errores = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errores.put(error.getField(), error.getDefaultMessage())
+        );
+
+        return ResponseEntity.badRequest().body(errores);
+    }
+
 }
