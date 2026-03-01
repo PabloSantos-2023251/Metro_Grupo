a720eaaf0801652f945b452ea68b32630ca92fa9
omartobar-2025092 <otobar-2025092@kinal.edu.gt>
2026-03-01 14:01:47 -0600

    feat: Se agrego la clase BoletoServiceImplements

---
+++ Backend/src/main/java/com/Metro/org/service/boletoserviceimplements.java
@@ -0,0 +1,53 @@
+package com.Metro.org.service;
+
+import com.Metro.org.entity.boleto;
+import com.Metro.org.repository.boletorepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class boletoserviceimplements implements boletoservice {
+    private final boletorepository boletoRepository;
+
+    public boletoserviceimplements(boletorepository boletoRepository) {
+        this.boletoRepository = boletoRepository;
+    }
+
+
+    @Override
+    public List<boleto> getAllBoletos() {
+        return boletoRepository.findAll();
+    }
+
+    @Override
+    public boleto getBoletoById(Integer id) {
+        return boletoRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public boleto saveBoleto(boleto boleto) throws RuntimeException {
+        return boletoRepository.save(boleto);
+    }
+
+    @Override
+    public boleto updateBoleto(Integer id, boleto boleto) {
+        Optional<boleto> boletoExistente = boletoRepository.findById(id);
+        if (boletoExistente.isPresent()) {
+            boleto newBoleto = boletoExistente.get();
+            newBoleto.setIdBoleto(boleto.getIdBoleto());
+            newBoleto.setFecha(boleto.getFecha());
+            newBoleto.setPrecio(boleto.getPrecio());
+            newBoleto.setPasajero(boleto.getPasajero());
+            return boletoRepository.save(newBoleto);
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public void deleteBoleto(Integer id){ boletoRepository.deleteById(id);
+
+    }
+}
