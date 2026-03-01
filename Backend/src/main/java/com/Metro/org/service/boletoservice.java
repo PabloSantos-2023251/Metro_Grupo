2363dbdc7afb1f0d5ee42d80cce145e3bc2d95ea
omartobar-2025092 <otobar-2025092@kinal.edu.gt>
2026-02-28 19:10:46 -0600

    feat: Se agrego boletos service

--- Backend/src/main/java/com/Metro/org/service/boletoservice.java
+++ Backend/src/main/java/com/Metro/org/service/boletoservice.java
@@ -1,4 +1,15 @@
 package com.Metro.org.service;
 
+import com.Metro.org.entity.boleto;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public interface boletoservice {
+    List<boleto> getAllBoletos();
+    boleto getBoletoById(Integer id);
+    boleto saveBoleto(boleto boleto) throws RuntimeException;
+    boleto updateBoleto(Integer id, boleto boleto);
+    void deleteBoleto(Integer id);
 }
