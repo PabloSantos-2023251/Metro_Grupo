7fb5f19b7f0e367f513e3ec7670bc173ae8185d6
DomingoRomero <dromero-2025157@kinal.edu.gt>
2026-04-23 21:48:44 -0600

    fix: se corrigio el boton eliminar

--- Backend/src/main/java/com/Metro/org/service/TrenServiceImplements.java
+++ Backend/src/main/java/com/Metro/org/service/TrenServiceImplements.java
@@ -1,19 +1,25 @@
 package com.Metro.org.service;
 
-
 import com.Metro.org.entity.Trenes;
 import com.Metro.org.repository.TrenRepository;
 import org.springframework.stereotype.Service;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 public class TrenServiceImplements implements TrenService {
+
     private final TrenRepository TrenRepository;
 
+    @PersistenceContext
+    private EntityManager entityManager;
 
     public TrenServiceImplements(TrenRepository trenRepository) {
-        TrenRepository = trenRepository;
+        this.TrenRepository = trenRepository;
     }
 
     @Override
@@ -31,25 +37,30 @@ public class TrenServiceImplements implements TrenService {
         return TrenRepository.save(trenes);
     }
 
-
-
     @Override
     public Trenes updateTren(Integer id, Trenes trenes) {
-        Trenes existingTrenes = TrenRepository.findById(id).orElseThrow(() -> new RuntimeException("El Tren no existe"));
+        Trenes existingTrenes = TrenRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("El Tren no existe"));
 
         existingTrenes.setModelo(trenes.getModelo());
         existingTrenes.setCapacidadPasajeros(trenes.getCapacidadPasajeros());
         existingTrenes.setEstado(trenes.getEstado());
 
         return TrenRepository.save(existingTrenes);
-
     }
 
     @Override
+    @Transactional
     public void deleteTren(Integer id) {
+
         if (!TrenRepository.existsById(id)) {
             throw new RuntimeException("Tren no existe");
         }
+
+        entityManager.createNativeQuery("DELETE FROM horarios WHERE id_tren = :id")
+                .setParameter("id", id)
+                .executeUpdate();
+
         TrenRepository.deleteById(id);
     }
 }
