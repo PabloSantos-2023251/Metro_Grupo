306e9b9973d6c75fd2f558458da456fc88f69f4d
MiguelSantizo <msantizo-2022021@kinal.edu.gt>
2026-04-29 01:29:01 -0600

    feat: se agregaron las entidades de pasajero

--- Backend/src/main/java/com/Metro/org/repository/PasajeroRepository.java
+++ Backend/src/main/java/com/Metro/org/repository/PasajeroRepository.java
@@ -3,7 +3,9 @@ package com.Metro.org.repository;
 import com.Metro.org.entity.Pasajero;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface PasajeroRepository extends JpaRepository<Pasajero, Integer> {
+    Optional<Pasajero> findByEmail(String email);
 }
