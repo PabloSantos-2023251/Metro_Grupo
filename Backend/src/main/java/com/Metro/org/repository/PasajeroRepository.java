17e44b60bd6c2b797de2096665b3fac25471aa29
omartobar-2025092 <otobar-2025092@kinal.edu.gt>
2026-05-05 07:57:01 -0600

    feat: Se agrego el findByEmail al reposiroty

--- Backend/src/main/java/com/Metro/org/repository/PasajeroRepository.java
+++ Backend/src/main/java/com/Metro/org/repository/PasajeroRepository.java
@@ -4,6 +4,9 @@ import com.Metro.org.entity.Pasajero;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PasajeroRepository extends JpaRepository<Pasajero, Integer> {
+    Optional<Pasajero> findByEmail(String email);
 }
